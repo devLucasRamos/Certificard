d4f9d470633dbd3239feef1aa3ba0adf3256dbf1
Lucas Ramos <87498874+devLucasRamos@users.noreply.github.com>
2021-09-24 17:27:44 -0300

    Aluramon!!

---
+++ ImersaoDev2021/aluramon-super-trunfo/README.markdown
@@ -0,0 +1,5 @@
+# Aluramon Super Trunfo
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/devlucasramos/pen/qBjKXMv](https://codepen.io/devlucasramos/pen/qBjKXMv).
+
+
---
+++ ImersaoDev2021/aluramon-super-trunfo/dist/script.js
@@ -0,0 +1,833 @@
+var Bulbasaur = {
+  nome: "Bulbasaur",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 6,
+    Esquiva: 8
+  }
+};
+
+var Squirtle = {
+  nome: "Squirtle",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/007.png",
+  atributos: {
+    AtqFisico: 6,
+    AtqEspecial: 8,
+    Esquiva: 7
+  }
+};
+
+var Charmander = {
+  nome: "Charmander",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/004.png",
+  atributos: {
+    AtqFisico: 7,
+    AtqEspecial: 7,
+    Esquiva: 5
+  }
+};
+var Pikachu = {
+  nome: "Pikachu",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/025.png",
+  atributos: {
+    AtqFisico: 3,
+    AtqEspecial: 9,
+    Esquiva: 8
+  }
+};
+var Caterpie = {
+  nome: "Caterpie",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/010.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 5,
+    Esquiva: 4
+  }
+};
+var Weedle = {
+  nome: "Weedle",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/013.png",
+  atributos: {
+    AtqFisico: 5,
+    AtqEspecial: 4,
+    Esquiva: 5
+  }
+};
+var Pidgey = {
+  nome: "Pidgey",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/016.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 6,
+    Esquiva: 7
+  }
+};
+var Rattata = {
+  nome: "Rattata",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/019.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 3,
+    Esquiva: 6
+  }
+};
+var Spearow = {
+  nome: "Spearow",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/021.png",
+  atributos: {
+    AtqFisico: 3,
+    AtqEspecial: 5,
+    Esquiva: 7
+  }
+};
+var Ekans = {
+  nome: "Ekans",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/023.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 7,
+    Esquiva: 5
+  }
+};
+var Sandshrew = {
+  nome: "Sandshrew",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/027.png",
+  atributos: {
+    AtqFisico: 7,
+    AtqEspecial: 4,
+    Esquiva: 5
+  }
+};
+var NidoranF = {
+  nome: "Nidoran F",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/029.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 5,
+    Esquiva: 3
+  }
+};
+var NidoranM = {
+  nome: "Nidoran M",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/032.png",
+  atributos: {
+    AtqFisico: 5,
+    AtqEspecial: 4,
+    Esquiva: 3
+  }
+};
+var Clefairy = {
+  nome: "Clefairy",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/035.png",
+  atributos: {
+    AtqFisico: 3,
+    AtqEspecial: 7,
+    Esquiva: 4
+  }
+};
+var Vulpix = {
+  nome: "Vulpix",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/037.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 8,
+    Esquiva: 5
+  }
+};
+var Jigglypuff = {
+  nome: "Jigglypuff",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/039.png",
+  atributos: {
+    AtqFisico: 3,
+    AtqEspecial: 5,
+    Esquiva: 4
+  }
+};
+var Zubat = {
+  nome: "Zubat",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/041.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 3,
+    Esquiva: 5
+  }
+};
+var Oddish = {
+  nome: "Oddish",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/043.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 5,
+    Esquiva: 3
+  }
+};
+var Paras = {
+  nome: "Paras",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/046.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 5,
+    Esquiva: 2
+  }
+};
+var Venonat = {
+  nome: "Venonat",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/048.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 7,
+    Esquiva: 3
+  }
+};
+
+var Diglett = {
+  nome: "Digglet",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/050.png",
+  atributos: {
+    AtqFisico: 7,
+    AtqEspecial: 4,
+    Esquiva: 3
+  }
+};
+
+var Meowth = {
+  nome: "Meowth",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/052.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 5,
+    Esquiva: 4
+  }
+};
+var Psyduck = {
+  nome: "Psyduck",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/054.png",
+  atributos: {
+    AtqFisico: 3,
+    AtqEspecial: 8,
+    Esquiva: 4
+  }
+};
+var Mankey = {
+  nome: "Mankey",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/056.png",
+  atributos: {
+    AtqFisico: 8,
+    AtqEspecial: 3,
+    Esquiva: 7
+  }
+};
+var Growlithe = {
+  nome: "Growlithe",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/058.png",
+  atributos: {
+    AtqFisico: 3,
+    AtqEspecial: 8,
+    Esquiva: 6
+  }
+};
+var Poliwag = {
+  nome: "Poliwag",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/060.png",
+  atributos: {
+    AtqFisico: 3,
+    AtqEspecial: 8,
+    Esquiva: 5
+  }
+};
+var Abra = {
+  nome: "Abra",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/063.png",
+  atributos: {
+    AtqFisico: 1,
+    AtqEspecial: 9,
+    Esquiva: 8
+  }
+};
+var Machop = {
+  nome: "Machop",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/066.png",
+  atributos: {
+    AtqFisico: 8,
+    AtqEspecial: 1,
+    Esquiva: 7
+  }
+};
+var Bellsprout = {
+  nome: "Bellsprout",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/069.png",
+  atributos: {
+    AtqFisico: 2,
+    AtqEspecial: 7,
+    Esquiva: 3
+  }
+};
+var Tentacool = {
+  nome: "Tentacool",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/072.png",
+  atributos: {
+    AtqFisico: 3,
+    AtqEspecial: 7,
+    Esquiva: 5
+  }
+};
+var Geodude = {
+  nome: "Geodude",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/074.png",
+  atributos: {
+    AtqFisico: 7,
+    AtqEspecial: 5,
+    Esquiva: 4
+  }
+};
+
+var Ponyta = {
+  nome: "Ponyta",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/077.png",
+  atributos: {
+    AtqFisico: 3,
+    AtqEspecial: 7,
+    Esquiva: 7
+  }
+};
+var Slowpoke = {
+  nome: "Slowpoke",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/079.png",
+  atributos: {
+    AtqFisico: 2,
+    AtqEspecial: 8,
+    Esquiva: 1
+  }
+};
+var Magnemite = {
+  nome: "Magnemite",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/081.png",
+  atributos: {
+    AtqFisico: 1,
+    AtqEspecial: 8,
+    Esquiva: 4
+  }
+};
+var Farfetch = {
+  nome: "Farfetch'd",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/083.png",
+  atributos: {
+    AtqFisico: 3,
+    AtqEspecial: 7,
+    Esquiva: 7
+  }
+};
+var Doduo = {
+  nome: "Doduo",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/084.png",
+  atributos: {
+    AtqFisico: 5,
+    AtqEspecial: 5,
+    Esquiva: 5
+  }
+};
+var Seel = {
+  nome: "Seel",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/086.png",
+  atributos: {
+    AtqFisico: 5,
+    AtqEspecial: 7,
+    Esquiva: 4
+  }
+};
+var Grimer = {
+  nome: "Grimer",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/088.png",
+  atributos: {
+    AtqFisico: 3,
+    AtqEspecial: 6,
+    Esquiva: 4
+  }
+};
+var Shellder = {
+  nome: "Shellder",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/090.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 6,
+    Esquiva: 5
+  }
+};
+var Gastly = {
+  nome: "Gastly",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/092.png",
+  atributos: {
+    AtqFisico: 1,
+    AtqEspecial: 8,
+    Esquiva: 9
+  }
+};
+var Onix = {
+  nome: "Onix",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/095.png",
+  atributos: {
+    AtqFisico: 9,
+    AtqEspecial: 6,
+    Esquiva: 5
+  }
+};
+var Drowzee = {
+  nome: "Drowzee",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/096.png",
+  atributos: {
+    AtqFisico: 1,
+    AtqEspecial: 8,
+    Esquiva: 4
+  }
+};
+var Krabby = {
+  nome: "Krabby",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/098.png",
+  atributos: {
+    AtqFisico: 7,
+    AtqEspecial: 6,
+    Esquiva: 5
+  }
+};
+var Voltorb = {
+  nome: "Voltorb",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/100.png",
+  atributos: {
+    AtqFisico: 5,
+    AtqEspecial: 6,
+    Esquiva: 4
+  }
+};
+var Exeggcute = {
+  nome: "Exeggcute",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/102.png",
+  atributos: {
+    AtqFisico: 3,
+    AtqEspecial: 7,
+    Esquiva: 5
+  }
+};
+var Cubone = {
+  nome: "Cubone",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/104.png",
+  atributos: {
+    AtqFisico: 6,
+    AtqEspecial: 7,
+    Esquiva: 5
+  }
+};
+var Hitmonlee = {
+  nome: "Hitmonlee",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/106.png",
+  atributos: {
+    AtqFisico: 9,
+    AtqEspecial: 4,
+    Esquiva: 8
+  }
+};
+var Hitmonchan = {
+  nome: "Hitmonchan",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/107.png",
+  atributos: {
+    AtqFisico: 8,
+    AtqEspecial: 4,
+    Esquiva: 9
+  }
+};
+var Lickitung = {
+  nome: "Lickitung",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/108.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 5,
+    Esquiva: 7
+  }
+};
+var Koffing = {
+  nome: "Koffing",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/109.png",
+  atributos: {
+    AtqFisico: 2,
+    AtqEspecial: 7,
+    Esquiva: 5
+  }
+};
+var Rhyhorn = {
+  nome: "Rhyhorn",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/111.png",
+  atributos: {
+    AtqFisico: 6,
+    AtqEspecial: 4,
+    Esquiva: 6
+  }
+};
+var Chansey = {
+  nome: "Chansey",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/113.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 7,
+    Esquiva: 4
+  }
+};
+var Tangela = {
+  nome: "Tangela",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/114.png",
+  atributos: {
+    AtqFisico: 3,
+    AtqEspecial: 7,
+    Esquiva: 3
+  }
+};
+var Kangaskhan = {
+  nome: "Kangaskhan",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/115.png",
+  atributos: {
+    AtqFisico: 7,
+    AtqEspecial: 5,
+    Esquiva: 3
+  }
+};
+var Horsea = {
+  nome: "Horsea",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/116.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 6,
+    Esquiva: 7
+  }
+};
+var Goldeen = {
+  nome: "Goldeen",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/118.png",
+  atributos: {
+    AtqFisico: 3,
+    AtqEspecial: 6,
+    Esquiva: 4
+  }
+};
+var Staryu = {
+  nome: "Staryu",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/120.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 5,
+    Esquiva: 5
+  }
+};
+var Growlite = {
+  nome: "Mew",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/151.png",
+  atributos: {
+    AtqFisico: 99,
+    AtqEspecial: 99,
+    Esquiva: 99
+  }
+};
+var MrMime = {
+  nome: "Mr. Mime",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/122.png",
+  atributos: {
+    AtqFisico: 2,
+    AtqEspecial: 8,
+    Esquiva: 5
+  }
+};
+var Scyther = {
+  nome: "Scyther",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/123.png",
+  atributos: {
+    AtqFisico: 6,
+    AtqEspecial: 6,
+    Esquiva: 9
+  }
+};
+var Jynx = {
+  nome: "Jynx",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/124.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 8,
+    Esquiva: 6
+  }
+};
+var Electabuzz = {
+  nome: "Electabuzz",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/125.png",
+  atributos: {
+    AtqFisico: 3,
+    AtqEspecial: 8,
+    Esquiva: 4
+  }
+};
+var Magmar = {
+  nome: "Magmar",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/126.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 8,
+    Esquiva: 3
+  }
+};
+var Pinsir = {
+  nome: "Pinsir",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/127.png",
+  atributos: {
+    AtqFisico: 7,
+    AtqEspecial: 6,
+    Esquiva: 3
+  }
+};
+var Tauros = {
+  nome: "Tauros",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/128.png",
+  atributos: {
+    AtqFisico: 6,
+    AtqEspecial: 6,
+    Esquiva: 7
+  }
+};
+var Magikarp = {
+  nome: "Magikarp",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/129.png",
+  atributos: {
+    AtqFisico: 1,
+    AtqEspecial: 1,
+    Esquiva: 1
+  }
+};
+var Lapras = {
+  nome: "Lapras",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/131.png",
+  atributos: {
+    AtqFisico: 4,
+    AtqEspecial: 7,
+    Esquiva: 5
+  }
+};
+var Eevee = {
+  nome: "Eevee",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/133.png",
+  atributos: {
+    AtqFisico: 6,
+    AtqEspecial: 6,
+    Esquiva: 6
+  }
+};
+var Porygon = {
+  nome: "Porygon",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/137.png",
+  atributos: {
+    AtqFisico: 2,
+    AtqEspecial: 8,
+    Esquiva: 3
+  }
+};
+var Omanyte = {
+  nome: "Omanyte",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/138.png",
+  atributos: {
+    AtqFisico: 6,
+    AtqEspecial: 5,
+    Esquiva: 3
+  }
+};
+var Kabuto = {
+  nome: "Kabuto",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/140.png",
+  atributos: {
+    AtqFisico: 7,
+    AtqEspecial: 7,
+    Esquiva: 2
+  }
+};
+var Dratini = {
+  nome: "Dratini",
+  imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/147.png",
+  atributos: {
+    AtqFisico: 5,
+    AtqEspecial: 7,
+    Esquiva: 6
+  }
+};
+
+var cartas = [
+  Bulbasaur,
+  Squirtle,
+  Charmander,
+  Pikachu,
+  Caterpie,
+  Weedle,
+  Pidgey,
+  Rattata,
+  Spearow,
+  Ekans,
+  Sandshrew,
+  NidoranF,
+  NidoranM,
+  Clefairy,
+  Vulpix,
+  Jigglypuff,
+  Zubat,
+  Oddish,
+  Paras,
+  Venonat,
+  Diglett,
+  Meowth,
+  Psyduck,
+  Mankey,
+  Growlithe,
+  Poliwag,
+  Abra,
+  Machop,
+  Bellsprout,
+  Tentacool,
+  Geodude,
+  Ponyta,
+  Slowpoke,
+  Magnemite,
+  Farfetch,
+  Doduo,
+  Seel,
+  Grimer,
+  Shellder,
+  Gastly,
+  Onix,
+  Drowzee,
+  Krabby,
+  Voltorb,
+  Growlite,
+  Exeggcute,
+  Cubone,
+  Hitmonlee,
+  Hitmonchan,
+  Lickitung,
+  Koffing,
+  Rhyhorn,
+  Chansey,
+  Tangela,
+  Kangaskhan,
+  Horsea,
+  Goldeen,
+  Staryu,
+  MrMime,
+  Scyther,
+  Jynx,
+  Electabuzz,
+  Magmar,
+  Pinsir,
+  Tauros,
+  Magikarp,
+  Lapras,
+  Eevee,
+  Porygon,
+  Omanyte,
+  Kabuto,
+  Dratini
+];
+
+var cartaMaquina;
+var cartaJogador;
+var length = cartas.length;
+
+function sortearCarta() {
+  var numeroCartaMaquina = parseInt(Math.random() * length);
+  cartaMaquina = cartas[numeroCartaMaquina];
+
+  var numeroCartaJogador = parseInt(Math.random() * length);
+  while (numeroCartaMaquina == numeroCartaJogador) {
+    var numeroCartaJogador = parseInt(Math.random() * length);
+  }
+  cartaJogador = cartas[numeroCartaJogador];
+  console.log(cartaJogador);
+
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+  exibirCartaJogador();
+}
+
+function obtemAtributoSelecionado() {
+  var radioAtributos = document.getElementsByName("atributo");
+
+  for (var i = 0; i < radioAtributos.length; i++) {
+    if (radioAtributos[i].checked == true) {
+      return radioAtributos[i].value;
+    }
+  }
+}
+
+function jogar() {
+  var atributoSelecionado = obtemAtributoSelecionado();
+  var divResultado = document.getElementById("resultado");
+  var elementoResultado = document.getElementById("resultado");
+
+  if (
+    cartaJogador.atributos[atributoSelecionado] >
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado =
+      "<p class='resultado-final'>Super Efetivo!!! VITORIA!!!</p>";
+  } else if (
+    cartaJogador.atributos[atributoSelecionado] <
+    cartaMaquina.atributos[atributoSelecionado]
+  ) {
+    htmlResultado =
+      "<p class='resultado-final'>Não foi muito efetivo... Você perdeu!</p>";
+  } else {
+    htmlResultado =
+      "<p class='resultado-final'>Ambos pokémon's desmaiaram!! EMPATE!!!</p>";
+  }
+  divResultado.innerHTML = htmlResultado;
+
+  document.getElementById("btnJogar").disabled = true;
+  exibirCartaMaquina();
+}
+
+function exibirCartaJogador() {
+  var divCartaJogador = document.getElementById("carta-jogador");
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
+  // divCartaJogador.style.backgroundImage = "url(" + cartaJogador.imagem + ")";
+  var moldura =
+    '<img src="https://i.pinimg.com/originals/c8/06/86/c80686b645efd1585f085e161cab6a34.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var opcoesTexto = "";
+  for (var atributo in cartaJogador.atributos) {
+    opcoesTexto +=
+      "<input type='radio' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaJogador.atributos[atributo] +
+      "<br>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaJogador.nome}</p>`;
+
+  divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
+
+function exibirCartaMaquina() {
+  var divCartaMaquina = document.getElementById("carta-maquina");
+  divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
+
+  var moldura =
+    '<img src="https://i.pinimg.com/originals/c8/06/86/c80686b645efd1585f085e161cab6a34.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>";
+
+  var opcoesTexto = "";
+  for (var atributo in cartaMaquina.atributos) {
+    opcoesTexto +=
+      "<p type='text' name='atributo' value='" +
+      atributo +
+      "'>" +
+      atributo +
+      " " +
+      cartaMaquina.atributos[atributo] +
+      "</p>";
+  }
+  var nome = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
+
+  divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>";
+}
